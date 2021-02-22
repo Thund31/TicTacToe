a3dab34c6b258c1607e6463c724fd0e26b974842
Siyang Qiu <qiusiyang100@gmail.com>
2021-02-22 12:54:53 +0800

    Game now supports two players versing each other

--- TicTacToe/Form1.Designer.cs
+++ TicTacToe/Form1.Designer.cs
@@ -30,12 +30,109 @@ namespace TicTacToe
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.lblTurn = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.btnComputer = new System.Windows.Forms.Button();
+            this.gbTiles = new System.Windows.Forms.GroupBox();
+            this.timerDrawTiles = new System.Windows.Forms.Timer(this.components);
+            this.lblResult = new System.Windows.Forms.Label();
+            this.timerPause1Sec = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblTurn
+            // 
+            this.lblTurn.AutoSize = true;
+            this.lblTurn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTurn.Location = new System.Drawing.Point(33, 17);
+            this.lblTurn.Name = "lblTurn";
+            this.lblTurn.Size = new System.Drawing.Size(42, 13);
+            this.lblTurn.TabIndex = 0;
+            this.lblTurn.Text = "Turn: X";
+            // 
+            // lblScore
+            // 
+            this.lblScore.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.Location = new System.Drawing.Point(134, 17);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(55, 13);
+            this.lblScore.TabIndex = 1;
+            this.lblScore.Text = "X 00:00 O";
+            // 
+            // btnComputer
+            // 
+            this.btnComputer.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnComputer.BackColor = System.Drawing.Color.Red;
+            this.btnComputer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnComputer.Location = new System.Drawing.Point(218, 12);
+            this.btnComputer.Name = "btnComputer";
+            this.btnComputer.Size = new System.Drawing.Size(94, 23);
+            this.btnComputer.TabIndex = 2;
+            this.btnComputer.Text = "Computer Player";
+            this.btnComputer.UseVisualStyleBackColor = false;
+            this.btnComputer.Click += new System.EventHandler(this.btnComputer_Click);
+            // 
+            // gbTiles
+            // 
+            this.gbTiles.AutoSize = true;
+            this.gbTiles.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.gbTiles.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.gbTiles.Location = new System.Drawing.Point(17, 54);
+            this.gbTiles.Name = "gbTiles";
+            this.gbTiles.Size = new System.Drawing.Size(6, 19);
+            this.gbTiles.TabIndex = 3;
+            this.gbTiles.TabStop = false;
+            this.gbTiles.Text = "0";
+            // 
+            // timerDrawTiles
+            // 
+            this.timerDrawTiles.Interval = 3000;
+            this.timerDrawTiles.Tick += new System.EventHandler(this.timerDrawTiles_Tick);
+            // 
+            // lblResult
+            // 
+            this.lblResult.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(69, 159);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(192, 63);
+            this.lblResult.TabIndex = 4;
+            this.lblResult.Text = "DRAW";
+            this.lblResult.Visible = false;
+            // 
+            // timerPause1Sec
+            // 
+            this.timerPause1Sec.Tick += new System.EventHandler(this.timerPause1Sec_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(335, 221);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.gbTiles);
+            this.Controls.Add(this.btnComputer);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.lblTurn);
+            this.Name = "Form1";
+            this.Text = "Tic Tac Toe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblTurn;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Button btnComputer;
+        private System.Windows.Forms.GroupBox gbTiles;
+        private System.Windows.Forms.Timer timerDrawTiles;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Timer timerPause1Sec;
     }
 }
 
--- TicTacToe/Form1.cs
+++ TicTacToe/Form1.cs
@@ -12,9 +12,259 @@ namespace TicTacToe
 {
     public partial class Form1 : Form
     {
+        private int TILE_SIZE = 100;
+        private String[] tokens = {"X", "O"};
+
+        private bool isXTurn = true, computerOn = false, performed;
+        private int numEmpty = 9, roundNum = 0, xScore = 0, oScore = 0;
+
+        private Button[,] tiles = new Button[3,3];
+
         public Form1()
         {
             InitializeComponent();
+
+            drawTiles();
+        }
+
+        private void drawTiles()
+        {
+            int x = 0, y = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Button newBtn = new Button();
+                    newBtn.Size = new Size(TILE_SIZE, TILE_SIZE);
+                    newBtn.Location = new Point(x, y);
+                    newBtn.Text = "";
+                    newBtn.FlatStyle = FlatStyle.Flat;
+                    newBtn.Font = new Font(FontFamily.GenericSansSerif, 60, FontStyle.Regular);
+                    newBtn.Click += btnTile_Click;
+
+                    gbTiles.Controls.Add(newBtn);
+                    tiles[i, j] = newBtn;
+
+                    x += TILE_SIZE;
+                }
+                x = 0;
+                y += TILE_SIZE;
+            }
+
+            if ((roundNum++) % 2 == 0)
+            {
+                isXTurn = true;
+                lblTurn.Text = "Turn: X";
+            }
+            else
+            {
+                isXTurn = false;
+                lblTurn.Text = "Turn: O";
+            }
+
+            numEmpty = 9;
+
+            computerMove();
+        }
+
+        private void btnTile_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            if (btn.Text != "") return;
+
+            if (isXTurn)
+            {
+                btn.Text = tokens[0];
+                isXTurn = false;
+                lblTurn.Text = "Turn: O";
+            }
+            else
+            {
+                btn.Text = tokens[1];
+                isXTurn = true;
+                lblTurn.Text = "Turn: X";
+            }
+
+            if (!checkWin() && --numEmpty == 0) drawGame();
+
+            computerMove();
+        }
+
+        private void computerMove()
+        {
+            if (!computerOn || isXTurn) return;
+
+            //先手
+            if (numEmpty == 9)
+            {
+                tiles[0, 0].PerformClick();
+            }
+            else if ((numEmpty == 7 || numEmpty == 5) && tiles[1, 1].Text == "")
+            {
+                if (tiles[2, 0].Text == "")
+                {
+                    tiles[2, 0].PerformClick();
+                }
+                else if (tiles[0, 2].Text == "")
+                {
+                    tiles[0, 2].PerformClick();
+                }
+                else if (tiles[2, 2].Text == "")
+                {
+                    tiles[2, 2].PerformClick();
+                }
+            }
+            //后手
+            else if (numEmpty == 8 && tiles[1, 1].Text == "")
+            {
+                tiles[1, 1].PerformClick();
+            }
+            else
+            {
+                scanTwo();
+            }
+            if (!isXTurn)
+            {
+                for (int i = 0; i < 3 && !performed; i++)
+                {
+                    for (int j = 0; j < 3 && !performed; j++)
+                    {
+                        if (tiles[i, j].Text == "")
+                        {
+                            tiles[i, j].PerformClick();
+                            performed = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void scanTwo()
+        {
+            performed = false;
+            for (int i = 0; i < 3 && !performed; i++)
+            {
+                if (!performed) checkTwo(i, 0, i, 1, i, 2, "x");
+                if (!performed) checkTwo(0, i, 1, i, 2, i, "x");
+            }
+            if (!performed) checkTwo(1, 1, 2, 2, 0, 0, "x");
+            if (!performed) checkTwo(0, 2, 1, 1, 2, 0, "x");
+
+            for (int i = 0; i < 3 && !performed; i++)
+            {
+                if (!performed) checkTwo(i, 0, i, 1, i, 2, "o");
+                if (!performed) checkTwo(0, i, 1, i, 2, i, "o");
+            }
+            if (!performed) checkTwo(1, 1, 2, 2, 0, 0, "o");
+            if (!performed) checkTwo(0, 2, 1, 1, 2, 0, "o");
+        }
+
+        private void checkTwo(int i1, int j1, int i2, int j2, int i3, int j3, string p)
+        {
+            if (tiles[i1, j1].Text == p && (tiles[i1, j1].Text == tiles[i2, j2].Text && tiles[i3, j3].Text == ""))
+            {
+                tiles[i3, j3].PerformClick();
+                performed = true;
+            }
+            if (tiles[i1, j1].Text == p && (tiles[i1, j1].Text == tiles[i3, j3].Text && tiles[i2, j2].Text == ""))
+            {
+                tiles[i2, j2].PerformClick();
+                performed = true;
+            }
+            if (tiles[i2, j2].Text == p && (tiles[i3, j3].Text == tiles[i2, j2].Text && tiles[i1, j1].Text == ""))
+            {
+                tiles[i1, j1].PerformClick();
+                performed = true;
+            }
+        }
+
+        private bool checkWin()
+        {
+            String token = tokens[1];
+            if (isXTurn) token = tokens[0];
+
+            bool win = false;
+
+            // horizontal
+            for (int i = 0; i < 3; i++)
+            {
+                if (tiles[i, 0].Text.Equals(token) && tiles[i, 1].Text.Equals(token) && tiles[i, 2].Text.Equals(token)) win = true;
+            }
+
+            // vertical
+            for (int j = 0; j < 3; j++)
+            {
+                if (tiles[0, j].Text.Equals(token) && tiles[1, j].Text.Equals(token) && tiles[2, j].Text.Equals(token)) win = true;
+            }
+
+            // diagonal
+            if (tiles[0, 0].Text.Equals(token) && tiles[1, 1].Text.Equals(token) && tiles[2, 2].Text.Equals(token)) win = true;
+            if (tiles[0, 2].Text.Equals(token) && tiles[1, 1].Text.Equals(token) && tiles[2, 0].Text.Equals(token)) win = true;
+
+            if (win)
+            {
+                winGame();
+                return true;
+            }
+            return false;
+        }
+
+        private void winGame()
+        {
+            if (!isXTurn)
+            {
+                lblResult.Text = "O WIN";
+                oScore++;
+            }
+            else
+            {
+                lblResult.Text = "X WIN";
+                xScore++;
+            }
+
+            lblScore.Text = "X " + xScore.ToString("D2") + ":" + oScore.ToString("D2") + " O";
+
+            timerPause1Sec.Enabled = true;
+        }
+
+        private void drawGame()
+        {
+            timerPause1Sec.Enabled = true;
+            lblResult.Text = "DRAW";
+        }
+
+        private void timerPause1Sec_Tick(object sender, EventArgs e)
+        {
+            roundNum++;
+            timerPause1Sec.Enabled = false;
+            gbTiles.Controls.Clear();
+            gbTiles.Visible = false;
+            lblResult.Visible = true;
+            timerDrawTiles.Enabled = true;
+        }
+
+        private void btnComputer_Click(object sender, EventArgs e)
+        {
+            if (computerOn)
+            {
+                computerOn = false;
+                btnComputer.BackColor = Color.Red;
+                computerMove();
+            } else
+            {
+                computerOn = true;
+                btnComputer.BackColor = Color.LimeGreen;
+            }
+        }
+
+        private void timerDrawTiles_Tick(object sender, EventArgs e)
+        {
+            lblResult.Visible = false;
+            gbTiles.Visible = true;
+            timerDrawTiles.Enabled = false;
+            drawTiles();
         }
     }
 }
